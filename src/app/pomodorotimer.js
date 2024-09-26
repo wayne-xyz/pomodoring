b51249f0233fdc09bd7b2dd87a5b548fd7230805
Lycopenee <309334941@qq.com>
2024-09-26 02:42:42 -0500

    updat the position of the  pomodorotimer

--- src/app/pomodorotimer.js
+++ src/app/pomodorotimer.js
@@ -59,7 +59,7 @@ export default function PomodoroTimer() {
 
   // Render the Pomodoro Timer component
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-background">
+    <div className="flex flex-col items-center justify-center bg-background h-[calc(100vh-4rem)]">
       {/* Circular progress bar */}
       <div className="relative w-64 h-64">
         <svg className="w-full h-full" viewBox="0 0 100 100">
