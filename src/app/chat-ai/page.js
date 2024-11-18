bafcc9782eef3e26d0b701264b287a5d656f03dd
Lycopenee <309334941@qq.com>
2024-11-18 04:38:18 -0600

    nothing change

--- src/app/chat-ai/page.js
+++ src/app/chat-ai/page.js
@@ -1,11 +1,11 @@
 'use client'
 
+import { useState } from 'react'
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Bot, Loader2 } from 'lucide-react'
 import Header from '../header'
-import { useState } from 'react'
 
 export default function ChatAIPage() {
   const [email, setEmail] = useState('')
